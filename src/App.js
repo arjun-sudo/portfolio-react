af25c080584479e823839af6f0ce5636a95c0647
arjun-sudo <laserarjun@gmail.com>
2020-09-04 19:32:26 +0545

    ui gredient and padding radius added

--- src/App.js
+++ src/App.js
@@ -7,8 +7,13 @@ function App() {
 
             <div className="container">
                 <div className="row">
-          <div className="col-3 app__sidebar">
-            Sidebar
+          <div className="col-3 ">
+              <div className="app__sidebar">
+                  sidebar
+              </div>
+
+
+
           </div>
           <div className="col-9 app__main-content">
             Main
