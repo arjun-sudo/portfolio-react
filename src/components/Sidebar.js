cbf86d37ce238839e2a2fcda7261d94455cbe9ba
arjun-sudo <laserarjun@gmail.com>
2020-09-05 06:19:01 +0545

    github account added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,55 +1,71 @@
 import React from "react";
-import fb from "../assets/fb.svg"
-import linkedin from "../assets/linkedin.svg"
-import resume from "../assets/resume.svg"
-import github from "../assets/github.svg"
-import insta from "../assets/insta.svg"
-import pin from "../assets/pin.svg"
-import avatar from "../assets/avatar.svg"
+import fb from "../assets/fb.svg";
+import linkedin from "../assets/linkedin.svg";
+import resume from "../assets/resume.svg";
+import github from "../assets/github.svg";
+import insta from "../assets/insta.svg";
+import pin from "../assets/pin.svg";
+import avatar from "../assets/avatar.svg";
+import GitHubButton from 'react-github-btn'
 
 const Sidebar = () => {
-    return (
-        <div className={"sidebar"}>
-            <img src={avatar} alt={"avatar"} className={"sidebar__avatar"}/>
-            <div className={"sidebar__name"}>
-                Arjun <span>Gautam</span>
-            </div>
-            <div className={"sidebar__item sidebar__title"}>Full Stack Developer</div>
-            <a href={"#"}>
-                <div className="sidebar__item">
-                    <img src={resume} alt={"resume"} className={"sidebar__icon"}/>Download
-                    Resume
-                </div>
-            </a>
-            <figure className={"sidebar__social-icons my-2"}>
-                <a href={""}>
-                    {" "}
-                    <img src={linkedin} alt={"linkedin"} className={"sidebar__icon mr-3"}/>
-                </a>
-                <a href={""}>
-                    <img src={fb} alt={"facebook"} className={"sidebar__icon mr-3"}/>
-                </a>
+  return (
+    <div className={"sidebar"}>
+      <img src={avatar} alt={"avatar"} className={"sidebar__avatar"} />
+      <div className={"sidebar__name"}>
+        Arjun <span>Gautam</span>
+      </div>
+      <div className={"sidebar__item sidebar__title"}>Full Stack Developer</div>
+      {/*<a href={"#"}>*/}
+      {/*<div className="sidebar__item sidebar__resume">*/}
+     <div>
+         <button className="btn">
+        <i className="fa fa-download"></i> Download Resume
+      </button>
+     </div>
 
-                <a href={""}>
-                    {" "}
-                    <img src={insta} alt={"instagram"} className={"sidebar__icon"}/>{" "}
-                </a>
-            </figure>
-            <div className="sidebar__contact">
-                <div className="sidebar__item">
-                    <a href={""}>
-                        <img src={github} alt={"github"} className={"sidebar__icon"}/>
-                    </a>
-                </div>
-                <div className="sidebar__location">
-                    <img src={pin} alt={""} className={"sidebar__icon"}/>
-                </div>
-                <div className="sidebar__item">laserarjun@gmail.com</div>
-                <div className={"sidebar__item"}>9863482273/9861616143</div>
-            </div>
-            <div className="sidebar__item sidebar__email">e-mail me</div>
+
+        {/*</div>*/}
+      {/*</a>*/}
+      {/*  <div className="sidebar__item sidebar__github">*/}
+      {/*      <a href={""}>*/}
+      {/*          <img src={github} alt={"github"} className={"sidebar__icon"} />*/}
+      {/*      </a>*/}
+      {/*      &nbsp;    Github*/}
+      {/*  </div>*/}
+      {/*  */}
+
+                <GitHubButton href="https://github.com/arjun-sudo" data-color-scheme="no-preference: dark; light: light; dark: dark;" data-size="large" aria-label="Follow @ntkme on GitHub">Follow @arjun-sudo</GitHubButton>
+      <div className="sidebar__contact">
+
+          <figure className={"sidebar__social-icons my-2"}>
+              <a href={""}>
+                  {" "}
+                  <img
+                      src={linkedin}
+                      alt={"linkedin"}
+                      className={"sidebar__icon mr-3"}
+                  />
+              </a>
+              <a href={""}>
+                  <img src={fb} alt={"facebook"} className={"sidebar__icon mr-3"} />
+              </a>
+
+              <a href={""}>
+                  {" "}
+                  <img src={insta} alt={"instagram"} className={"sidebar__icon"} />{" "}
+              </a>
+          </figure>
+        <div className="sidebar__location">
+          <img src={pin} alt={""} className={"sidebar__icon"} />
+          Kathmandu, Nepal
         </div>
-    );
+        <div className="sidebar__item">laserarjun@gmail.com</div>
+        <div className={"sidebar__item"}>9863482273/9861616143</div>
+      </div>
+      <div className="sidebar__item sidebar__email">email me</div>
+    </div>
+  );
 };
 
 export default Sidebar;
