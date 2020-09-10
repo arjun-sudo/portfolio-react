8b645350e4f2ba9d6b8a55dd86a475f6dadd8158
arjun-sudo <laserarjun@gmail.com>
2020-09-10 20:06:12 +0545

    Project card added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import data from './data/projects_data'
+import ProjectCard from './ProjectCard'
 const Projects = () => {
     return (
        <div className="container projects">
            <div className="row">
                {
+
+                   data.map(project=>
+                       <ProjectCard key={project.name} project={project}/>)
+                   })
 
                }
            </div>
--- src/components/data/projects_data.js
+++ src/components/data/projects_data.js
@@ -1,7 +1,34 @@
+import employee from "src/assets/projects/typing.png"
 
 
 const data_projects=[
     {
-        name:''
-    }
+        name:'Employee Management System',
+        image:employee,
+        deployed_url:'',
+        github_url:'https://github.com/arjun-sudo/EmployeManagementSystem',
+        category:['react.js','spring']
+    },
+    {
+        name:'Smart ATM System',
+        image:employee,
+        deployed_url:'',
+        github_url:'',
+        category:['react.js','spring']
+    },
+    {
+        name:'Typing Test',
+        image:employee,
+        deployed_url:'https://lasertyping.netlify.app/',
+        github_url:'',
+        category:['react.js','spring']
+    },
+    {
+        name:'Employee Management System',
+        image:employee,
+        deployed_url:'',
+        github_url:'',
+        category:['react.js','spring']
+    },
+
 ]
