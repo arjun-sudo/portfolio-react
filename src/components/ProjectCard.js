189249b255688bfdc11547fdb638c18c55e7f1b1
arjun-sudo <laserarjun@gmail.com>
2020-09-11 20:14:25 +0545

    Project Card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import github from '/home/arjun/Personal Project/portfolio-react/src/assets/github.svg'
-const ProjectCard = ({projects:name,image,deployed_url,github_url}) => {
+import github from '../assets/github.svg'
+const ProjectCard = ({ project: { name, image, deployed_url, github_url } }) => {
     return (
-        <div>
-            <figure className="projectCard__wrapper col-md-6 col-lg-4">
-                <a href={deployed_url}>
-                    <img src={image} alt={"deployed url"} className={"projectCard__image"}/>
+        <div className="projectCard col-md-6 col-lg-4 my-2">
+            <figure className="projectCard__wrapper">
+                <a href={deployed_url} target="_blank">
+                    <img src={image} alt={name} className="projectCard__image" />
                 </a>
                 <div className="projectCard__title">
-                <a href={github_url}>
-                    <img src={github} alt={"github link"} className={"projectCard__image"}/>
-                </a>
+                    <a href={github_url} target="_blank">
+                        <img src={github} alt="github link" className="projectCard__icon" />
+                    </a>
+                    {name}
                 </div>
-
-
             </figure>
+
         </div>
     );
 };
